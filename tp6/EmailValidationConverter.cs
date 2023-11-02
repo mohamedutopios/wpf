3f91211d030b5aba9104a0fd2973690ce48d7120
mohamedutopios <mohamed@utopios.net>
2023-11-02 16:31:17 +0100

    add correction TP6

---
+++ tp6/EmailValidationConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace tp6
+{
+    public class EmailValidationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string email)
+            {
+                string pattern = @"^[^@\s]+@[^@\s]+\.(fr)$"; // Vérifie si l'e-mail contient '@' et se termine par '.fr'
+                return Regex.IsMatch(email, pattern);
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tp6/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tp6
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSubmitButtonClick(object sender, RoutedEventArgs e)
+        {
+            bool isEmailValid = (bool)(new EmailValidationConverter().Convert(emailTextBox.Text, null, null, null));
+
+            if (string.IsNullOrWhiteSpace(nameTextBox.Text) && isEmailValid)
+            {
+                validationLabel.Content = "Le nom est obligatoire!";
+            }
+            else if (!isEmailValid && !string.IsNullOrWhiteSpace(nameTextBox.Text))
+            {
+                validationLabel.Content = "L'email est invalide!";
+
+            } else if (string.IsNullOrWhiteSpace(nameTextBox.Text) && !isEmailValid)
+            {
+                validationLabel.Content = "Le nom est obligatoire et l'email est invalide!";
+            }
+            else
+            {
+                validationLabel.Content = "Inscription réussie!";
+                validationLabel.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Green);
+            }
+        }
+    }
+}
